c6c021b604ec99c52a8467e8928373f52aa15bc0
Kevin Manson <kevin.manson@qohash.com>
2022-11-18 00:16:40 -0500

    chore: initial commit
    Release-As: 0.0.1

---
+++ src/Tabs.tsx
@@ -0,0 +1,174 @@
+import React, {
+  useCallback,
+  useEffect,
+  useId,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+import { getTabProps } from './helpers/childrenUtils'
+import {
+  isTabElement,
+  isTabListElement,
+  isTabPanelElement,
+} from './helpers/elementTypes'
+import type { TabProps } from './Tab'
+
+export type TabsComponent<T> = ((props: T) => JSX.Element) & {
+  tabsRole: string
+}
+
+export interface TabsProps {
+  children: React.ReactNode
+  onSelect?: (index: number, lastIndex: number) => void
+  onNameSelect?: (name?: string, lastName?: string) => void
+  selected?: number | string
+}
+
+const Tabs = ({ children, onNameSelect, onSelect, selected }: TabsProps) => {
+  const id = useId()
+  const tabProps = useMemo(() => getTabProps(children), [children])
+  const tabNames = useMemo(() => tabProps.map((tab) => tab.name), [tabProps])
+  const tabIds = useRef(tabProps.map((_, i) => `${id}-${i}`))
+  const [{ index: currentIndex, name: currentName }, setSelected] = useState(
+    computeState(tabNames, selected)
+  )
+
+  useEffect(() => {
+    setSelected(computeState(tabNames, selected))
+  }, [selected, tabNames])
+
+  const handleSelect = useCallback(
+    (index: number, name?: string) => {
+      setSelected({
+        index,
+        name,
+      })
+      onSelect?.(index, currentIndex)
+      onNameSelect?.(name, currentName)
+    },
+    [currentIndex, currentName, onNameSelect, onSelect]
+  )
+
+  const selectNext = useCallback(() => {
+    if (tabProps.every((tab) => tab.disabled)) {
+      return
+    }
+
+    const nextIndex = getNextIndex(currentIndex, tabProps)
+    const nextName = tabNames[nextIndex]
+
+    handleSelect(nextIndex, nextName)
+  }, [currentIndex, handleSelect, tabNames, tabProps])
+
+  const selectPrevious = useCallback(() => {
+    if (tabProps.every((tab) => tab.disabled)) {
+      return
+    }
+
+    const nextIndex = getPreviousIndex(currentIndex, tabProps)
+    const nextName = tabNames[nextIndex]
+
+    handleSelect(nextIndex, nextName)
+  }, [currentIndex, handleSelect, tabNames, tabProps])
+
+  const getChildren = useCallback(() => {
+    let tabPanelIndex = 0
+
+    const idsToCreate = tabNames.length - tabIds.current.length
+    tabIds.current.push(
+      ...Array.from(new Array(idsToCreate)).map(
+        (_, index) => `${id}-${tabNames.length + index - 1}`
+      )
+    )
+
+    return React.Children.map(children, (child) => {
+      if (isTabListElement(child)) {
+        return React.cloneElement(child, {
+          ...child.props,
+          children: React.Children.map(child.props.children, (tab, index) => {
+            if (isTabElement(tab)) {
+              return React.cloneElement(tab, {
+                ...tab.props,
+                active: index === currentIndex,
+                id: `${tabIds.current[index]}-tab`,
+                'aria-controls': `${tabIds.current[index]}-panel`,
+                onClick: () => handleSelect(index, tab.props.name),
+              })
+            }
+
+            return tab
+          }),
+          onArrowLeftKeyDown: selectPrevious,
+          onArrowRightKeyDown: selectNext,
+        })
+      }
+
+      if (isTabPanelElement(child)) {
+        const panelIndex = Number.isInteger(child.props.index)
+          ? (child.props.index as number)
+          : tabPanelIndex
+        const panelElement = React.cloneElement(child, {
+          active: panelIndex === currentIndex,
+          id: `${tabIds.current[panelIndex]}-panel`,
+          'aria-labelledby': `${tabIds.current[panelIndex]}-tab`,
+        })
+
+        tabPanelIndex += 1
+
+        return panelElement
+      }
+
+      return child
+    })
+  }, [
+    children,
+    currentIndex,
+    handleSelect,
+    id,
+    selectNext,
+    selectPrevious,
+    tabNames.length,
+  ])
+
+  return <>{getChildren()}</>
+}
+
+function computeState(
+  tabNames: (string | undefined)[],
+  selected?: number | string
+): {
+  index: number
+  name?: string
+} {
+  if (Number.isInteger(selected)) {
+    return { index: selected as number, name: tabNames[selected as number] }
+  }
+  if (selected) {
+    return {
+      index: tabNames.indexOf(selected as string),
+      name: selected as string,
+    }
+  }
+  return { index: 0, name: tabNames[0] }
+}
+
+function getNextIndex(currentIndex: number, tabProps: TabProps[]) {
+  let nextIndex = currentIndex < tabProps.length - 1 ? currentIndex + 1 : 0
+  while (tabProps[nextIndex].disabled) {
+    nextIndex = nextIndex < tabProps.length - 1 ? nextIndex + 1 : 0
+  }
+
+  return nextIndex
+}
+
+function getPreviousIndex(currentIndex: number, tabProps: TabProps[]) {
+  let previousIndex = currentIndex > 0 ? currentIndex - 1 : tabProps.length - 1
+  while (tabProps[previousIndex].disabled) {
+    previousIndex = previousIndex > 0 ? previousIndex - 1 : tabProps.length - 1
+  }
+
+  return previousIndex
+}
+
+export default Tabs
